cdfd6daf6ff447d46fa74d54a6ce51451107dacf
ShitpostingAlt <manumanish434@gmail.com>
2022-10-09 15:53:03 +0530

    Added HOME page

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path"
+import { dirname } from 'path';
 import {
   GetTrendingVideosOkXXX,
   GetPopularPornstar,
@@ -7,57 +9,59 @@ import {
   GetHanimeWeeklyTop,
   // GetPornstarsBykeyword,
   GetWeeklyHamster,
-  GetRandom
+  GetRandom,
 } from "./scraper/routes.js";
-const app = express();
 import cors from "cors";
+const app = express()
 const port = process.env.PORT || 4000;
+
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.get("/gambit/getTop", async (req, res) => {
-  const page = req.query.page
-  const data = await GetPopularPornstar({page : page});
+  const page = req.query.page;
+  const data = await GetPopularPornstar({ page: page });
   res.send(data);
 });
 app.get("/gambit/popularxxx", async (req, res) => {
-  const page = req.query.page
+  const page = req.query.page;
 
-  const data = await GetPopUlarVideosOkXXX({page : page});
+  const data = await GetPopUlarVideosOkXXX({ page: page });
   res.send(data);
 });
 app.get("/gambit/trendingxxx", async (req, res) => {
-  const page = req.query.page
+  const page = req.query.page;
 
-  const data = await GetTrendingVideosOkXXX({page : page});
+  const data = await GetTrendingVideosOkXXX({ page: page });
   res.send(data);
 });
 app.get("/gambit/search", async (req, res) => {
-  const keyw = req.query.keyw
-  const page = req.query.page
-  console.log(`https://ok.xxx/search/${keyw}/${page}/`)
-  const data = await GetbyKeyword({ keyw: keyw, page: page })
-  res.send(data)
-})
+  const keyw = req.query.keyw;
+  const page = req.query.page;
+  console.log(`https://ok.xxx/search/${keyw}/${page}/`);
+  const data = await GetbyKeyword({ keyw: keyw, page: page });
+  res.send(data);
+});
 app.get("/gambit/hanime", async (req, res) => {
-  const data = await GetHanimeWeeklyTop({})
-  res.send(data)
-})
+  const data = await GetHanimeWeeklyTop({});
+  res.send(data);
+});
 // app.get("/gambit/tiavapornstars", async (req, res) => {
 //   const keyw = req.query.keyw
 //   const data = await GetPornstarsBykeyword({})
 //   res.send(data)
 //})
 app.get("/gambit/weeklybest", async (req, res) => {
-  const page = req.query.page
-  const data = await GetWeeklyHamster({ page:page })
-  res.send(data)
-})
+  const page = req.query.page;
+  const data = await GetWeeklyHamster({ page: page });
+  res.send(data);
+});
 app.get("/gambit/random", async (req, res) => {
-  const data = await GetRandom({})
-  res.send(data)
-})
+  const data = await GetRandom({});
+  res.send(data);
+});
+
 app.get("/", async (req, res) => {
-  res.send("XDDDD");
+  res.sendFile( "index.html", { root: "views" });
 });
 
 app.listen(port, () => {
